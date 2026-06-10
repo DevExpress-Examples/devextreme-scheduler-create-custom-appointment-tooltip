17aa80b7d3263b697542317277977c9de3f32c36
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-10 18:47:31 +0300

    Implement Angular example

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
-import { DxButtonModule, DxButtonTypes } from 'devextreme-angular/ui/button';
+import { DxSchedulerModule, DxSchedulerTypes } from 'devextreme-angular/ui/scheduler';
+import { DxListModule } from 'devextreme-angular/ui/list';
+import { AppointmentInfoComponent, TooltipItem } from './appointment-info.component';
+import { data, employees } from './data';
 
 @Component({
     selector: 'app-root',
-    imports: [DxButtonModule],
+    imports: [DxSchedulerModule, DxListModule, AppointmentInfoComponent],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
+  dataSource = data;
 
-  counter = 0;
+  resourcesDataSource = employees;
 
-  buttonText = 'Click count: 0';
+  currentDate = new Date(2021, 5, 2);
 
-  onClick(_e: DxButtonTypes.ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  tooltipItems: TooltipItem[] = [];
+
+  onAppointmentTooltipShowing(e: DxSchedulerTypes.AppointmentTooltipShowingEvent): void {
+    e.cancel = true;
+    const { appointments } = e;
+    this.tooltipItems = (appointments ?? []).map((item) => ({
+      text: item.appointmentData.text,
+      colorDef: item.color,
+      startDate: this.formatDate(item.appointmentData.startDate as Date),
+      endDate: this.formatDate(item.appointmentData.endDate as Date),
+    }));
+  }
+
+  private formatDate(date: Date): string {
+    return `${date.getHours()}:${date.getMinutes()}`;
   }
 }
---
+++ Angular/src/app/appointment-info.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface TooltipItem {
+  text?: string;
+  colorDef: PromiseLike<string>;
+  startDate: string;
+  endDate: string;
+}
+
+@Component({
+  selector: 'appointment-info',
+  template: `
+    <div class="tooltip">
+      <div class="marker" [style.background]="color"></div>
+      <div>
+        <p>{{ data.text }}</p>
+        <p>{{ data.startDate }} - {{ data.endDate }}</p>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .tooltip {
+        display: flex;
+        align-items: baseline;
+      }
+
+      .marker {
+        width: 10px;
+        height: 10px;
+        margin-right: 10px;
+        border-radius: 50%;
+      }
+    `,
+  ],
+})
+export class AppointmentInfoComponent implements OnInit {
+  @Input() data!: TooltipItem;
+
+  color = '';
+
+  ngOnInit(): void {
+    this.data.colorDef.then((color) => {
+      this.color = color;
+    });
+  }
+}
